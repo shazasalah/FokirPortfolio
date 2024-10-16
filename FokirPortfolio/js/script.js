0c3179feb319f71cb1012252d913c7b9db347d64
shazasalah <89835064+shazasalah@users.noreply.github.com>
2024-10-16 15:25:41 +0300

    Add files via upload

---
+++ FokirPortfolio/js/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel({
+//         items:1,
+//         margin:30,
+//         dots:true,
+//         // nav:true,
+//         loop:true,
+//         autoplay:true,
+//         autoplayTimeout:1000
+//     });
+//   });
